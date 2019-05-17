07097286eb1aa8cfe13959e8d3c88f9a119c6226
Alecx Moritz <alecxmoritz7@gmail.com>
2019-05-16 22:09:17 -0400

    added form & onChange handers

--- src/components/apps/nyt-app/NytApp.js
+++ src/components/apps/nyt-app/NytApp.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+const baseURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+const key = 'yourKeyHereabc123def456ghi789jkl0';
 
 const NytApp = () => {
   // state
@@ -19,20 +21,23 @@ const NytApp = () => {
       .catch(err => console.log(err));
   };
 
-
+  handleSubmit = (event) => {
+    event.preventDefault();
+    fetchResults();
+  }
 
   return (
     <div className="main">
       <div className="mainDiv">
-        <form onSubmit={}>
+        <form onSubmit={(e) => handleSubmit(e)}>
           <span>Enter a single search term (required) : </span>
-          <input type="text" name="search" onChange={} required />
+          <input type="text" name="search" onChange={(e) => setSearch(e.target.value)} required />
           <br />
           <span>Enter a start date: </span>
-          <input type="date" name="startDate" pattern="[0-9]{8}" onChange={} />
+          <input type="date" name="startDate" pattern="[0-9]{8}" onChange={(e) => setStateDate(e.target.value)} />
           <br />
           <span>Enter an end date: </span>
-          <input type="date" name="endDate" pattern="[0-9]{8}" onChange={} />
+          <input type="date" name="endDate" pattern="[0-9]{8}" onChange={(e) => setEndDate(e.target.value)} />
           <br />
           <button className="submit">Submit search</button>
         </form>
