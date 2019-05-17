8c6cd5cb89aacab3ad06da2e1fc40aa3f385fadc
Alecx Moritz <alecxmoritz7@gmail.com>
2019-05-16 21:55:16 -0400

    added fetchResults()

--- src/components/apps/nyt-app/NytApp.js
+++ src/components/apps/nyt-app/NytApp.js
@@ -8,13 +8,26 @@ const NytApp = () => {
   const [ pageNumber, setPageNumber ] = useState(0);
   const [ results, setResults ] = useState([]); 
 
+  fetchResults = () => {
+    let url = `${baseURL}?api-key=${key}&page=${this.state.pageNumber}&q=${this.state.search}`;
+    url = this.state.startDate ? url + `&begin_date=${this.state.startDate}` : url;
+    url = this.state.endDate ? url + `&end_date=${this.state.endDate}` : url;
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setResults(data.response.docs))
+      .catch(err => console.log(err));
+  };
+
+
+
   return (
     <div className="main">
       <div className="mainDiv">
-
+        
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default NytApp;
