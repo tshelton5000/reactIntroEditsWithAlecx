27c5e072b5ccf398a2b18ad73660be987d09f89e
Alecx Moritz <alecxmoritz7@gmail.com>
2019-05-16 22:18:22 -0400

    added article display

--- src/components/apps/nyt-app/NytResults.js
+++ src/components/apps/nyt-app/NytResults.js
@@ -4,7 +4,25 @@ const NytResults = (props) => {
 
   return (
     <div>
-      
+      <div>
+        { props.results.map(result => {
+          return (
+            <div key={result._id}>
+            <h2>{result.headline.main}</h2>
+            {result.multimedia.length > 1 ? <img alt="article" src={`http://www.nytimes.com/${result.multimedia[1].url}`} /> : ''}
+            <p>
+              {result.snippet}
+              <br />
+              {result.keywords.length > 0 ? ' Keywords: ' : ''}
+            </p>
+            <ul>
+              {result.keywords.map(keyword => <li key={keyword.value}>{keyword.value}</li>)}
+            </ul>
+            <a href={result.web_url}><button>Read It</button></a>
+          </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
