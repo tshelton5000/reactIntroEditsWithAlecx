c1c4628a06cfd62c4ddce3e6b78f7e1c0f13058d
Alecx Moritz <alecxmoritz7@gmail.com>
2019-05-16 22:46:05 -0400

    added style state values and style prop

--- src/components/concepts/PropsDemo.js
+++ src/components/concepts/PropsDemo.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PropsDemo = () => {
-    
+    const [ color, setColor ] = useState('white');
+    const [ backgroundColor, setBackgroundColor ] = useState('purple');
+    const [ borderRadius, setBorderRadius ] = useState('5px');
+    const [ borderStyle, setBorderStyle ] = useState('dashed');
+    const [ display, setDisplay ] = useState('inline-block');
+    const [ width, setWidth ] = useState('350px');
+    const [ textAlign, setTextAlign ] = useState('center');
+
+    let styles = {
+        color : color,
+        backgroundColor : backgroundColor,
+        borderRadius : borderRadius,
+        borderStyle : borderStyle,
+        display : display,
+        width : width,
+        textAlign : textAlign
+    }
+
     return (
-        <div>
-            <FunctionalComponent />
+        <div className="main">
+            <div className="mainDiv">
+                <div style={ styles }>
+                    <FunctionalComponent string="will this display?" />
+                    {/* challenge one answer */}
+                    <FunctionalComponent string="Props are pretty cool right?" />
+                    <FunctionalComponent string="You can send data from one component ..." />
+                    <FunctionalComponent string=" ... to another!" />
+                </div>
+            </div>
         </div>
     );
 };
